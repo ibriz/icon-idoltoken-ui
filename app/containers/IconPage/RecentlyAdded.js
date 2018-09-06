818b078215051072aca44e313dedd78cf0788c5a
sagarduwal <sagarduwal0102@hotmail.com>
2018-09-06 19:54:50 +0545

    Added static personalities to Recently Added

--- app/containers/IconPage/RecentlyAdded.js
+++ app/containers/IconPage/RecentlyAdded.js
@@ -4,6 +4,9 @@ import defaultIdol from 'assets/default.jpg';
 import ariana from 'assets/adriana_grande.png';
 import selena from 'assets/salena_gomez.jpg';
 import taylor from 'assets/taylor_swift.png';
+import bobice from 'assets/bo-bice.png';
+import carrie from 'assets/carrie.png';
+
 function RecentlyAdded (props) {
   return (
     <Grid.Column>
@@ -25,7 +28,24 @@ function RecentlyAdded (props) {
         </div>
         <div className="clear"></div>
       </div>
+      <div className="media">
+        <img style={{width: '60px'}} src={bobice}/>
+        <div className="media-content">
+          <h3 className="media-content-text" >Bo Bice</h3>
+          <button className="getBtn">GET</button>
+        </div>
+        <div className="clear"></div>
+      </div>
+      <div className="media">
+        <img style={{width: '60px'}} src={carrie}/>
+        <div className="media-content">
+          <h3 className="media-content-text" >Carrie Underwood</h3>
+          <button className="getBtn">GET</button>
+        </div>
+        <div className="clear"></div>
+      </div>
     </Grid.Column>
+
   )
 }
 
